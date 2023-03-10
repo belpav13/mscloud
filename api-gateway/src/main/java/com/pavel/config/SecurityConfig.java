4fe5dc3eed794a0f346ea2c387f6a59b68366642
pasha <belpav@gmail.com>
2023-03-10 21:04:45 +0200

    config

--- api-gateway/src/main/java/com/pavel/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/pavel/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.pavel.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -22,4 +25,11 @@ public class SecurityConfig {
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
         return serverHttpSecurity.build();
     }
+
+//    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+//    private String issuerUri;
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder() {
+//        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
+//    }
 }
